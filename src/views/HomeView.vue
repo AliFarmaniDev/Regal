c41f9ab7aaf6c24327298bec0d1f61209ae170bf
Alireza <alirezafarmani619@gmail.com>
2024-11-30 22:16:18 -0800

    install axios,json-server and tailwindCss and start the project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2 class="text-red-700">home page</h2>
+  </div>
 </template>
